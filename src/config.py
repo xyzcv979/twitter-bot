0e118371f75f779acfc8e881f631f68e67feb290
alex <avon1462@gmail.com>
2021-07-10 18:51:30 -0400

    testing .gitignore

--- src/config.py
+++
@@ -1,9 +0,0 @@
-# This file holds API keys
-# .gitignore should include reference to config.py
-
-CONSUMER_KEY = "jsUUbnzrd78eK671kkKCmvbyc"
-CONSUMER_SECRET = "1uoZxsyrN5Zes0eLAXO1M6UBNRpWhkpTgFEakeTZEzTWBKStiG"
-ACCESS_TOKEN = "1413957314189606915-B3nkRtjtNQPHQsDGsXqnDpmumYoHpL"
-ACCESS_TOKEN_SECRET = "KhXkUsFScyPIu5IC2yqlB2DFvuLDLxMz5KyDi4QZGw6hT"
-
-WEATHER_KEY = "c988549e4ee3e5258e46c820b7dddd84"
